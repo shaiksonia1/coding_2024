264f2fb436dc931b29980bb0277a8633ea271d76
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-23 17:34:20 +0530

    Operators_Python

--- python_24/03_operators.py
+++ python_24/03_operators.py
@@ -1,4 +1,5 @@
 # Operators in python
+
 # Arthemetic Operators
 
 a = 9 + 3
@@ -9,4 +10,24 @@ c = 6 * 6
 
 d = 5 % 2
 
-print(a,b,c,c,d)
+# print(a,b,c,c,d)
+
+# Assignment Operators
+a = 4-2 #Assign 4-2 in a
+print(a)
+b = 6
+b +=3  # Increment the value of b by 3 then assign it to b
+
+print(b)
+
+c = 7
+c *=3 # mutliply the value of c by 3 then assign it to c
+print(c)
+
+d = 4
+d  /=5 # Divide the value of b by 4 then assign it to d
+print(d)
+
+e = 8
+e -=3   # Decrement the value of b by 3 then assign it to b
+print(e)
