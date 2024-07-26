61429e6349f182887dbcaace5ec3468c1eede82a
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-27 01:49:14 +0530

    practice

---
+++ python_24/practice1.py
@@ -0,0 +1,13 @@
+# write a program to add two numbers
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+print("Sum of ab is", a + b)
+
+
+# write a program to find a remainder when a number is divide by z.
+
+c = int(input("Enter a number:"))
+d = int(input("Enter a number:"))
+
+print(" division of ab is", c % d)
