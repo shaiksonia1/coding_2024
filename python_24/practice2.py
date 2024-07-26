26d6373a53dcd037eb03a90e15a52315fd723576
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-27 03:46:02 +0530

    Practice

---
+++ python_24/practice2.py
@@ -0,0 +1,28 @@
+# problem1
+# name = input("what is your name?")
+# print( f"Good Afternoon! {name}")
+
+#  problem 2
+
+letter = '''
+Dear<|Name|>,
+You are selected!
+<|Date|>'''
+
+print(letter.replace("<|Name|>" , "Sonia" ).replace("<|Date|>" , "27th July 2024"))
+
+
+# problem 3
+
+name = "shaik  Sonia"
+
+print(name.find("  "))
+
+# problem 4
+name = "shaik  Sonia"
+
+print(name.replace("  ", " "))
+
+# problem 5
+
+
