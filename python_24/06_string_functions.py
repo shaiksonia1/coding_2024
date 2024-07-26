ed7dc402321cf8d5c6a8a4e5c8b5ea6599a53358
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-27 03:47:50 +0530

    Strings

---
+++ python_24/06_string_functions.py
@@ -0,0 +1,26 @@
+name = "Ali Raja"
+print(len(name))
+
+print(name.endswith("ja"))
+print(name.startswith("ja"))
+print(name.capitalize())
+print(name.title())
+
+s = "helloworld"
+
+index = s.replace("hello" , "Namaste")
+print(index)
+
+# espcape,new line
+# new line
+a = "sonia is a beautiful \nand smart girl "
+print(a)
+# single quote
+b = "sonia is a beautiful and 'smart' girl "
+print(b)
+# double quote
+c = "sonia is a beautiful and \"smart\" girl "
+print(c)
+d = 'sonia is a beautiful and \'smart\' girl '
+print(d)
+
