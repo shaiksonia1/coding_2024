98f3e998b6b5b458349d168df115363e070c455b
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-27 01:48:52 +0530

    operators

--- python_24/03_operators.py
+++ python_24/03_operators.py
@@ -15,9 +15,9 @@ d = 5 % 2
 # Assignment Operators
 a = 4-2 #Assign 4-2 in a
 print(a)
+
 b = 6
 b +=3  # Increment the value of b by 3 then assign it to b
-
 print(b)
 
 c = 7
@@ -31,3 +31,47 @@ print(d)
 e = 8
 e -=3   # Decrement the value of b by 3 then assign it to b
 print(e)
+
+
+#comparison Operators
+f = 2<5
+print(f)
+
+g = 5>4
+print(g)
+
+h = 9<= 7
+print(h)
+
+i = 4>=6
+print(i)
+
+j = 7==7
+print(j)
+
+k = 4!=3
+print(k)
+
+#Logical Operatora
+
+l = True or False
+print(l)
+
+# Truth table of 'OR'
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or False is", False or False)
+print("False or True is", False or True)
+
+
+# Truth Table of 'And'
+
+print("True and False is" ,True and False)
+print("False and False is" ,False and False)
+print("False and True is" ,False and True)
+print("True and True is" ,True and True)
+
+# Not Operator
+print(not(False))
+print(not(True))
+
