f2c19e551991d4e8f29e020fdc2f85c8e7e8d41c
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-29 02:29:46 +0530

    Basics_of_Python

---
+++ python_24/Basics_of_Python/08_Tuple.py
@@ -0,0 +1,17 @@
+#Tuple is immutable data type in python
+#we use ( ) circle brackets where as in lists we use this[ ]
+a = () # empty tuple
+print(a)
+
+b = (1,23,4,5,6,7,8,565,745)
+print(b)
+print(type(b))
+
+c = (2,)
+print(c) #single element tuple
+
+num=("sonia",3,55,"Ankit",4353,"insha",534,"Raja")
+print(num)
+print(num.index(3))
+print(num.count(55))
+print(len(num))
