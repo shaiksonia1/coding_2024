5786440f027a8de9ab6cb9c8dde427566782c6ea
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-29 02:23:52 +0530

    Lists

--- python_24/07_Lists.py
+++ python_24/07_Lists.py
@@ -10,11 +10,10 @@ print(rawdata[2])
 # Lists Methods
 num = [1,2,3,4,5,6,7,8,9]
 print(num)
-num.append(10)
-num.pop(1)
-num.insert(6,27)
-num.insert(5,80)
-num.sort()
-num.reverse()
-num.remove(4)
+num.append(10) #will add the element in the end
+num.pop(1) # will remove the element
+num.insert(6,27) #on the 6th position it will add 27
+num.sort() #it will sort in ascending order
+num.reverse() #it will reverse the list
+num.remove(4)  # it will remove particular element
 print(num)
