eee561a6b5e4fb9e35eee12000c2ad8902ca3c7c
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-29 02:29:19 +0530

    changes

--- python_24/04_Typecasting.py
+++
@@ -1,8 +0,0 @@
-a = 31
-t = type(a)
-print(t)
-
-b = "9.45"
-d = type(b)
-print(d)
-
--- python_24/main.py
+++
@@ -1,9 +0,0 @@
-import pyjokes
-
-#  this module is used for technical jokes
-# this module is used for technical jokes
-
-""" this is  amultiline comment
-I would tell you a joke about UDP, but you would never get it."""
-joke = pyjokes.get_joke()
-print(joke)
