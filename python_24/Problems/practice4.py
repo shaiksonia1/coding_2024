78a131b1c2b8f770697b4a537c927ab819624b73
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-30 02:12:52 +0530

    problems

---
+++ python_24/Problems/practice4.py
@@ -0,0 +1,65 @@
+# #p1
+# words = {
+#     "hello" : "namaste",
+#     "good":"achha",
+#     "how":"kaise",
+#     "why" : "kyu"
+# }
+# word = input("enter the word you want the meaning of : ")
+# print(words[word])
+
+# #p2
+# s = set()
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+# user = input("enter the  numbers")
+# s.add(int(user))
+
+# print(s)
+
+#p3
+
+# s = {"18",18}
+# print(s)
+
+#p4
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+print(s)
+print(len(s))
+
+#p5
+s ={} # type of this will be dictionary not set 
+
+
+d = {}
+
+name = input("enter name : " )
+lang = input("enter language : ")
+d.update({name:lang})
+name = input("enter name : " )
+lang = input("enter language : ")
+d.update({name:lang})
+name = input("enter name : " )
+lang = input("enter language : ")
+d.update({name:lang})
+name = input("enter name : " )
+lang = input("enter language : ")
+d.update({name:lang})
+
+print(d)
+
