aee4031f339d58d58fe7db2dab65158e85ba3b83
Shaik Sonia <shaiksonia222@gmail.com>
2024-07-31 16:52:59 +0530

    problems

--- python_24/Problems/practice8.py
+++ python_24/Problems/practice8.py
@@ -32,3 +32,30 @@ def sum(n):
         return 1
     return sum(n-1)+n
 print(sum(n))
+
+# p4
+
+n = int(input("enter a number"))
+def pattern(n):
+    if(n==0):
+        return 0
+    print("*" * n )
+    pattern(n-1)
+  
+pattern(n)
+
+#p5
+def inch_cms(inch):
+    return inch*2.54
+
+n = int(input("enter a number"))
+print(f"the value in cms  to inch is {inch_cms(n)}")
+
+n = int(input("enter a number"))
+
+def multiplication():
+   for i in range(1,11):
+        print(f"{n}x{i} = {n*i}")
+
+multiplication()
+
