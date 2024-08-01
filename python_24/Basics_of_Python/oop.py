3c33e8164ef0235080e941caf2f7a5ec4bfe4ce4
Shaik Sonia <shaiksonia222@gmail.com>
2024-08-02 01:47:41 +0530

    Basics_of_Pyhon

---
+++ python_24/Basics_of_Python/oop.py
@@ -0,0 +1,17 @@
+#OBJECT ORIENTED PROGRAMMING
+
+#class :Class is a blueprint for creating an object
+class Cars:
+    model = "BMW"
+    color = "White"
+    price = 5000000
+
+
+    
+BMW = Cars()
+print(BMW.model,BMW.color)
+
+maruti = Cars()
+print(maruti.price,maruti.color)
+
+#object: 
