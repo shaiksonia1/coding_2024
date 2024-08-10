edd307d57d404c825ce440f04ab6014b251cd54c
Shaik Sonia <shaiksonia222@gmail.com>
2024-08-11 03:57:03 +0530

    Basics_of_Python

---
+++ python_24/Basics_of_Python/24_enumerate.py
@@ -0,0 +1,10 @@
+l =[2,5456,7,8,87,36,]
+
+# index =0 
+# for item in l:
+#     print(f"the item number at index {index}is {item}")
+#     index +=1
+
+# but we can do this by enumerate 
+for index, item in enumerate(l):
+    print(f"the item number at index {index}is {item}")
