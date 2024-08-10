980a97d80252fef4b6fb75f2b581d7c072ad2f97
Shaik Sonia <shaiksonia222@gmail.com>
2024-08-11 03:59:49 +0530

    Basics_of_Python

--- python_24/Basics_of_Python/24_enumerate.py
+++ python_24/Basics_of_Python/24_enumerate.py
@@ -8,3 +8,15 @@ l =[2,5456,7,8,87,36,]
 # but we can do this by enumerate 
 for index, item in enumerate(l):
     print(f"the item number at index {index}is {item}")
+
+
+#list Comphrensions
+mylist = [3,345,8,9876543,5678]
+
+# squaredlist = []
+# for item in mylist:
+#     squaredlist.append(item*item)
+
+squaredlist = [i*i for i in mylist]
+
+print(squaredlist)
