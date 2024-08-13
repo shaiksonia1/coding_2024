70d25e4009b6c4daa9efdb8086ff613224ca392b
Shaik Sonia <shaiksonia222@gmail.com>
2024-08-14 03:16:34 +0530

    Hackerrank_Solutions

---
+++ Python_solutions_Hackerrank/soltion3_arthmetic_operation.py
@@ -0,0 +1,15 @@
+# Task
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+# Example
+
+
+num1 = int(input("enter a number"))
+num2 = int(input("enter a number"))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
